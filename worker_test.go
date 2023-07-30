b50b777f73783957af092248f4382e4eb46a1259
Parham Alvani <parham.alvani@gmail.com>
2023-07-30 07:17:48 +0330

    feat: add tests for worker

---
+++ worker_test.go
@@ -0,0 +1,40 @@
+package koi_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/1995parham/koi"
+)
+
+func TestNewWorker(t *testing.T) {
+	t.Parallel()
+
+	printer := func(n int) koi.NoReturn {
+		return koi.None
+	}
+
+	cases := []struct {
+		concurrentCount int64
+		expectedErr     error
+	}{
+		{
+			concurrentCount: 10,
+			expectedErr:     nil,
+		},
+		{
+			concurrentCount: 1,
+			expectedErr:     nil,
+		},
+		{
+			concurrentCount: 0,
+			expectedErr:     koi.ErrMinConcurrentCount,
+		},
+	}
+
+	for _, c := range cases {
+		if _, err := koi.NewWoker(printer, 10, c.concurrentCount); !errors.Is(err, c.expectedErr) {
+			t.Error("worker creation failed")
+		}
+	}
+}
